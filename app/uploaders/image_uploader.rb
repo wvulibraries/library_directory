fb6b24efcf08d505f0e6c172044c04fb3878bd4f
ddavisgraphics <dave@ddavisgraphics.com>
2018-07-26 21:48:14 -0400

    rubo cop trailing whitespace.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -24,7 +24,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   process resize_to_fit: [800, 800]
-  
+
   # Create different versions of your uploaded files:
   version :small do
     process resize_to_fit: [300, 300]
