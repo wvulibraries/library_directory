cb620ae21ff4c52284d690735f8c801c59cf1343
ddavisgraphics <dave@ddavisgraphics.com>
2018-08-07 15:18:35 -0400

    Add flash types for menus

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
+  # base setups
   protect_from_forgery with: :exception
+  add_flash_types :success, :warning, :danger, :info, :error
+
   layout 'application'
 
   # concerns
