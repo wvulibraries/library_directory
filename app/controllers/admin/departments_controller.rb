dd8dc93f30cae21bf13a2c42b86ab7a93ae681f9
ddavisgraphics <dave@ddavisgraphics.com>
2018-08-14 14:56:35 -0400

    Starting Departments.

--- app/controllers/admin/departments_controller.rb
+++ app/controllers/admin/departments_controller.rb
@@ -1,3 +1,79 @@
+# Departments Controller
+# @author David J. Davis
+# Sets data for views, sets redirects, sets errors
 class Admin::DepartmentsController < AdminController
-  def index; end
+
+  # before_actions
+  before_action :set_department, only: %i[show edit update destroy]
+
+  # GET admin/departments
+  # GET admin/departments.json
+  def index
+    @departments = Department.all.includes(:building).order(:name)
+  end
+
+  # GET /departments/1
+  # GET /departments/1.json
+  def show; end
+
+  # GET /departments/new
+  def new
+    @department = Department.new
+    @buildings =  Building.all.order(:name)
+  end
+
+  # GET /departments/1/edit
+  def edit; end
+
+  # POST /departments
+  # POST /departments.json
+  def create
+    @department = Department.new(department_params)
+    respond_to do |format|
+      if @department.save
+        format.html { redirect_to admin_department_path(@department), success: I18n.t('department.success') }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  # PATCH/PUT /departments/1
+  # PATCH/PUT /departments/1.json
+  def update
+    respond_to do |format|
+      if @department.update(department_params)
+        format.html { redirect_to admin_department_path(@department), success: I18n.t('department.edited') }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  # DELETE /departments/1
+  # DELETE /departments/1.json
+  def destroy
+    @building.destroy
+    respond_to do |format|
+      format.html { redirect_to '/admin/departments', success: I18n.t('building.deleted') }
+    end
+  end
+
+  # Private Methods
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_department
+    @department = Department.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def department_params
+    params.require(:department)
+    .permit(  :building,
+              :name,
+              :description,
+              service_points_attributes: [:id, :name, phone_attributes: [:id, :number_types, :number, :_destroy]]
+            )
+  end
 end
