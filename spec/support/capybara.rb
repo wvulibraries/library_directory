a2617dec10214e42c2621c380808d1a8b084d245
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2019-04-25 14:40:42 -0400

    cleanup work
    removed travis files and reset docker-compose.yml back to original settings. also removed unneeded lines in capybara.rb.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -19,36 +19,16 @@ Capybara.register_driver :remote_browser do |app|
   )
 end
 
-# Capybara.javascript_driver = :remote_browser
+Capybara.javascript_driver = :remote_browser
 
-# RSpec.configure do |config|
-#   config.before(:each) do
-#     Capybara.app_host = 'http://rails:3001'
-#   end
+RSpec.configure do |config|
+  config.before(:each) do
+    Capybara.app_host = 'http://rails:3001'
+  end
 
-#   config.after(:each) do
-#     Capybara.reset_sessions!
-#     Capybara.use_default_driver
-#     Capybara.app_host = nil
-#   end
-# end
-
-# TRAVIS CAPYBARA
-# ============================================================
-# require 'capybara/webkit'
-
-# Capybara.javascript_driver = :webkit
-
-# Capybara::Webkit.configure do |config|
-#   # Enable debug mode. Prints a log of everything the driver is doing.
-#   # config.debug = true
-#   # Allow pages to make requests to any URL without issuing a warning.
-#   config.allow_unknown_urls
-#   config.ignore_ssl_errors
-#   config.skip_image_loading
-#   config.raise_javascript_errors = false
-# end
-
-# CIRCLECI CAPYBARA
-# ============================================================
-
+  config.after(:each) do
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+    Capybara.app_host = nil
+  end
+end
