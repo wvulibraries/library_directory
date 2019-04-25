3b3252d3cb9fd84bcee66931696a11e3534a7e1c
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2019-04-25 13:43:31 -0400

    testing circleci and capybara

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,23 +1,23 @@
 # LOCAL CAPYBARA
 # ============================================================
-# require 'capybara'
+require 'capybara'
 
-# Capybara.run_server = true
-# Capybara.server_host = '0.0.0.0'
-# Capybara.server_port = 3001
+Capybara.run_server = true
+Capybara.server_host = '0.0.0.0'
+Capybara.server_port = 3001
 
-# Capybara.register_driver :remote_browser do |app|
-#   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-#     chromeOptions: { args: %w[headless disable-gpu no-sandbox] }
-#   )
+Capybara.register_driver :remote_browser do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w[headless disable-gpu no-sandbox] }
+  )
 
-#   Capybara::Selenium::Driver.new(
-#     app,
-#     browser: :remote,
-#     url: 'http://selenium:4444/wd/hub',
-#     desired_capabilities: capabilities
-#   )
-# end
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :remote,
+    url: 'http://selenium:4444/wd/hub',
+    desired_capabilities: capabilities
+  )
+end
 
 # Capybara.javascript_driver = :remote_browser
 
@@ -51,34 +51,4 @@
 
 # CIRCLECI CAPYBARA
 # ============================================================
-require 'capybara'
 
-Capybara.run_server = true
-Capybara.server_host = '0.0.0.0'
-Capybara.server_port = 3001
-
-Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w(headless disable-gpu) }
-  )
-
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    desired_capabilities: capabilities
-  )
-end
-
-Capybara.javascript_driver = :headless_chrome
-
-RSpec.configure do |config|
-  config.before(:each) do
-    Capybara.app_host = 'http://rails:3001'
-  end
-
-  config.after(:each) do
-    Capybara.reset_sessions!
-    Capybara.use_default_driver
-    Capybara.app_host = nil
-  end
-end
