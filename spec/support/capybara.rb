214e75920e3af068f1acd0b23b4cd8ddf501616b
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2019-04-25 11:44:48 -0400

    local capybara change
    testing changes

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,50 +1,50 @@
-# # LOCAL CAPYBARA
-# # ============================================================
-# require 'capybara'
+# LOCAL CAPYBARA
+# ============================================================
+require 'capybara'
 
-# Capybara.run_server = true
-# Capybara.server_host = '0.0.0.0'
-# Capybara.server_port = 3001
+Capybara.run_server = true
+Capybara.server_host = '0.0.0.0'
+Capybara.server_port = 3001
 
-# Capybara.register_driver :remote_browser do |app|
-#   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-#     chromeOptions: { args: %w[headless disable-gpu no-sandbox] }
-#   )
+Capybara.register_driver :remote_browser do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w[headless disable-gpu no-sandbox] }
+  )
 
-#   Capybara::Selenium::Driver.new(
-#     app,
-#     browser: :remote,
-#     url: 'http://selenium:4444/wd/hub',
-#     desired_capabilities: capabilities
-#   )
-# end
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :remote,
+    url: 'http://selenium:4444/wd/hub',
+    desired_capabilities: capabilities
+  )
+end
 
-# Capybara.javascript_driver = :remote_browser
+Capybara.javascript_driver = :remote_browser
 
-# RSpec.configure do |config|
-#   config.before(:each) do
-#     Capybara.app_host = 'http://rails:3001'
-#   end
+RSpec.configure do |config|
+  config.before(:each) do
+    Capybara.app_host = 'http://rails:3001'
+  end
 
-#   config.after(:each) do
-#     Capybara.reset_sessions!
-#     Capybara.use_default_driver
-#     Capybara.app_host = nil
-#   end
-# end
+  config.after(:each) do
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+    Capybara.app_host = nil
+  end
+end
 
 # TRAVIS CAPYBARA
 # ============================================================
-require 'capybara/webkit'
+# require 'capybara/webkit'
 
-Capybara.javascript_driver = :webkit
+# Capybara.javascript_driver = :webkit
 
-Capybara::Webkit.configure do |config|
-  # Enable debug mode. Prints a log of everything the driver is doing.
-  # config.debug = true
-  # Allow pages to make requests to any URL without issuing a warning.
-  config.allow_unknown_urls
-  config.ignore_ssl_errors
-  config.skip_image_loading
-  config.raise_javascript_errors = false
-end
+# Capybara::Webkit.configure do |config|
+#   # Enable debug mode. Prints a log of everything the driver is doing.
+#   # config.debug = true
+#   # Allow pages to make requests to any URL without issuing a warning.
+#   config.allow_unknown_urls
+#   config.ignore_ssl_errors
+#   config.skip_image_loading
+#   config.raise_javascript_errors = false
+# end
