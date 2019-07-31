1987e214845b2ad8b88b26f46fa6d94c58cc3c5e
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2019-07-31 16:21:53 -0400

    corrected path in startup.sh

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@
 export TERM=vt100
 
 # remove PID and start the server
-file="/home/databases/tmp/pids/server.pid"
+file="/home/directory/tmp/pids/server.pid"
 [ -f $file ] && rm $file
 
 bin/rails s -p 3000 -b '0.0.0.0'
